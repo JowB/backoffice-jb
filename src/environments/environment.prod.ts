11172ec9a91328445d5fe6eb5f86fcb75fcb1956
Bretton <jbretton@entoria.fr>
2021-07-27 11:02:53 +0200

    Add serverUrl for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import {audience, clientId, domain} from '../../auth_config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain,
+    clientId,
+    redirectUri: window.location.origin,
+    audience
+  },
+  serv: {
+    serverUrl: 'https://api-jb.herokuapp.com/'
+  }
 };
